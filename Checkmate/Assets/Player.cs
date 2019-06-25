4e252f8307f81717db870d1a8717cd495d5ec03d
tfritzy <tfritzy@gmail.com>
2019-06-24 21:33:22 -0700

    Added a player class that has attributes of a playername and a postion

---
+++ Checkmate/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player {
+
+    public string PlayerName;
+    public int xPos;
+    public int yPos;
+
+    public Player(string name)
+    {
+        this.PlayerName = name;
+        this.xPos = 0;
+        this.yPos = 0;
+    }
+	
+}
